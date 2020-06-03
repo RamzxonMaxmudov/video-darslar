b385ae42dd35a4cf98a25454598608bd5806509d
Maxmudov Ramzxon <devramzxon@gmail.com>
2020-06-03 11:38:04 +0500

    Birinchi commit

---
+++ Test_Git_Project/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Test_Git_Project
+//
+//  Created by IOS Developer on 6/3/20.
+//  Copyright © 2020 IOS Developer. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Test_Git_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_Git_Project
+//
+//  Created by IOS Developer on 6/3/20.
+//  Copyright © 2020 IOS Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
